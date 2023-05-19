94a51b1686b929357edcb57699380287537d17df
Tarwat <t.uddin9121@gmail.com>
2023-05-19 19:55:04 +0100

    feat(notepad): added 'export' feature for markdown text

--- src/components/demo/Notepad/Notepad.tsx
+++ src/components/demo/Notepad/Notepad.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { PaperDownload } from 'react-iconly';
 
-import { TextEditor } from '@/components/ui';
+import { TextEditor, Button } from '@/components/ui';
 import { useLocalStorage } from '@/hooks';
+import { generatePDF } from '@/utils';
 import { LOCAL_STORAGE_SAVE_MS } from '@/config/constants';
 
 const Notepad: React.FC = () => {
@@ -21,13 +23,27 @@ const Notepad: React.FC = () => {
   }, [value, setMdText]);
 
   return (
-    <div className="w-10/12 max-w-md mx-auto md:max-w-lg lg:w-6/12 lg:max-w-2xl min-h-[372px]">
-      <TextEditor
-        value={value}
-        onChange={setValue}
-        preview="edit"
-        height={372}
-      />
+    <div>
+      <div className="w-10/12 max-w-md mx-auto md:max-w-lg lg:w-6/12 lg:max-w-2xl min-h-[372px]">
+        <TextEditor
+          value={value}
+          onChange={setValue}
+          preview="edit"
+          height={372}
+        />
+      </div>
+      <Button
+        size="lg"
+        className="block mx-auto mt-16 mb-12"
+        leadIcon={<PaperDownload />}
+        onClick={() => {
+          const { parseMarkdown, exportPDF } = generatePDF();
+          parseMarkdown(mdText);
+          exportPDF('demo-thoughts.pdf');
+        }}
+      >
+        Export
+      </Button>
     </div>
   );
 };
