eebbc09d38da93ced347c6ecb7caec85b84f6a0e
Tarwat <t.uddin9121@gmail.com>
2023-05-25 20:32:36 +0100

    feat(pages): added dashboard page for root route

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Journals from '@/components/dashboard/Journals';
+
+const Dashboard = () => (
+  <main className="min-h-screen p-12">
+    <div>Dashboard</div>
+    <Journals />
+  </main>
+);
+
+export default Dashboard;
