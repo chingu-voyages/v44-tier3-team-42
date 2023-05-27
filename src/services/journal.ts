ed3c985fe43ac39cd3fad0203ef05b454270f0f2
Tarwat <t.uddin9121@gmail.com>
2023-05-27 19:12:31 +0100

    feat(services): added new schemas, methods and other types for journal service

--- src/services/journal.ts
+++ src/services/journal.ts
@@ -2,18 +2,55 @@ import { z } from 'zod';
 
 import { SERVER_URL } from '@/config/constants';
 
-type ErrorObject = {
+type ErrorResponse = {
   message: string;
 };
 
+type SuccessResponse = {
+  message: string;
+};
+
+/// ///////////////////////////////////
+/// //////// SCHEMAS
+/// ///////////////////////////////////
+
 const journalReferenceSchema = z.object({
+  id: z.number(),
+  user_id: z.number(),
   journal_title: z.string(),
   cover_image: z.string(),
 });
 
-export type JournalReference = z.infer<typeof journalReferenceSchema>;
+export type JournalReference = Partial<z.infer<typeof journalReferenceSchema>>;
 
-export const getJournals = async (): Promise<JournalReference[]> => {
+const journalSectionSchema = z.object({
+  id: z.number(),
+  journal_reference_id: z.number(),
+  section_number: z.number(),
+});
+
+export type JournalSection = Partial<z.infer<typeof journalSectionSchema>>;
+
+const journalContentSchema = z.object({
+  id: z.number(),
+  journal_section_id: z.number(),
+  content: z.string(),
+});
+
+export type JournalContent = Partial<z.infer<typeof journalContentSchema>>;
+
+/// ///////////////////////////////////
+/// //////// METHODS
+/// ///////////////////////////////////
+
+const GetJournalsResponseSchema = journalReferenceSchema.omit({
+  id: true,
+  user_id: true,
+});
+
+type GetJournalsResponse = z.infer<typeof GetJournalsResponseSchema>;
+
+export const getJournals = async (): Promise<GetJournalsResponse[]> => {
   const res = await fetch(`${SERVER_URL}/browse-journals`, {
     method: 'GET',
     credentials: 'include',
@@ -21,11 +58,57 @@ export const getJournals = async (): Promise<JournalReference[]> => {
   const data = await res.json();
 
   if (!res.ok) {
-    throw new Error((data as ErrorObject).message);
+    throw new Error((data as ErrorResponse).message);
   }
 
   // Check to see if JSON object in the data array is valid
   journalReferenceSchema.parse(data[0]);
+
+  return data;
+};
+
+const GetJournalByNameResponseSchema = journalReferenceSchema.pick({
+  id: true,
+});
+
+type GetJournalByNameResponse = z.infer<typeof GetJournalByNameResponseSchema>;
+
+export const getJournalByName = async (
+  title: string,
+): Promise<GetJournalByNameResponse> => {
+  const req = { title };
+  const res = await fetch(`${SERVER_URL}/journal-with-name`, {
+    method: 'GET',
+    credentials: 'include',
+    body: JSON.stringify(req),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error((data as ErrorResponse).message);
+  }
+
+  // Check to see if JSON object in the data array is valid
+  journalReferenceSchema.parse(data);
+
+  return data;
+};
+
+export const updateJournal = async (
+  id: number,
+  journalEntry: string,
+): Promise<SuccessResponse> => {
+  const req = { journalId: id, journalEntry };
+  const res = await fetch(`${SERVER_URL}/save-journal`, {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify(req),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error((data as ErrorResponse).message);
+  }
 
   return data;
 };
