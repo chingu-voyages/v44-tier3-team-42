0e976e7b73775671ad28208c70f48915fd143cea
Tarwat <t.uddin9121@gmail.com>
2023-05-27 19:10:56 +0100

    feat(constants): added MEDIA_QUERY_BREAKPOINTS constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,9 @@
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 export const LOCAL_STORAGE_SAVE_MS = 1000;
+export const MEDIA_QUERY_BREAKPOINTS = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px',
+} as const;
