4b43d9ed6ce9d99bd1fcb0a7be323cdbc8fb7ae9
Tarwat <t.uddin9121@gmail.com>
2023-05-29 20:05:52 +0100

    feat(services): added createJournal service

--- src/services/journal.ts
+++ src/services/journal.ts
@@ -1,19 +1,56 @@
-import { z } from 'zod';
+import z from 'zod';
 
 import { SERVER_URL } from '@/config/constants';
 
-type ErrorObject = {
+type ErrorResponse = {
   message: string;
 };
 
+type SuccessResponse = {
+  message: string;
+};
+
+/// ///////////////////////////////////
+/// //////// SCHEMAS
+/// ///////////////////////////////////
+
 const journalReferenceSchema = z.object({
+  id: z.number(),
+  user_id: z.number(),
   journal_title: z.string(),
   cover_image: z.string(),
 });
 
 export type JournalReference = z.infer<typeof journalReferenceSchema>;
 
-export const getJournals = async (): Promise<JournalReference[]> => {
+const journalSectionSchema = z.object({
+  id: z.number(),
+  journal_reference_id: z.number(),
+  section_number: z.number(),
+});
+
+export type JournalSection = z.infer<typeof journalSectionSchema>;
+
+const journalContentSchema = z.object({
+  id: z.number(),
+  journal_section_id: z.number(),
+  content: z.string(),
+});
+
+export type JournalContent = z.infer<typeof journalContentSchema>;
+
+/// ///////////////////////////////////
+/// //////// METHODS
+/// ///////////////////////////////////
+
+const getJournalsResponseSchema = journalReferenceSchema.omit({
+  id: true,
+  user_id: true,
+});
+
+type GetJournalsResponse = z.infer<typeof getJournalsResponseSchema>;
+
+export const getJournals = async (): Promise<GetJournalsResponse[]> => {
   const res = await fetch(`${SERVER_URL}/browse-journals`, {
     method: 'GET',
     credentials: 'include',
@@ -21,11 +58,39 @@ export const getJournals = async (): Promise<JournalReference[]> => {
   const data = await res.json();
 
   if (!res.ok) {
-    throw new Error((data as ErrorObject).message);
+    throw new Error((data as ErrorResponse).message);
   }
 
   // Check to see if JSON object in the data array is valid
   journalReferenceSchema.parse(data[0]);
+
+  return data;
+};
+
+export const createJournalRequestSchema = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(1, { message: 'Include at least a single letter :¬)' })
+    .max(50, { message: 'Something a bit shorter please :D' }),
+  url: z.string().url({ message: 'Must provide a valid image url' }),
+});
+
+export type CreateJournalRequest = z.infer<typeof createJournalRequestSchema>;
+
+export const createJournal = async (
+  newJournal: CreateJournalRequest,
+): Promise<SuccessResponse> => {
+  const res = await fetch(`${SERVER_URL}/create-journal`, {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify(newJournal),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error((data as ErrorResponse).message);
+  }
 
   return data;
 };
