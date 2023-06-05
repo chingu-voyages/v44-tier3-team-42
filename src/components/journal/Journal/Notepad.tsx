08821bbbb40c36e2fe4802e04e09b6e5aefb614c
Tarwat <t.uddin9121@gmail.com>
2023-06-05 21:28:23 +0100

    feat(components): added 'onSave' callback for Notepad component

--- src/components/journal/Journal/Notepad.tsx
+++ src/components/journal/Journal/Notepad.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
 import rehypeSanitize from 'rehype-sanitize';
 
-import { TextEditor } from '@/components/ui';
+import { Button, TextEditor } from '@/components/ui';
 import useLocalStorage from '@/hooks/useLocalStorage';
 import { LOCAL_STORAGE_SAVE_MS } from '@/config/constants';
 
 type Props = {
   id: string | number;
   initialText?: string;
+  onSave?: (text: string) => void;
 };
 
-const Notepad: React.FC<Props> = ({ id, initialText = '' }) => {
+const Notepad: React.FC<Props> = ({ id, initialText = '', onSave }) => {
   const [mdText, setMdText] = useLocalStorage(
     `app:notepad-md-${id}`,
     initialText,
@@ -27,16 +28,31 @@ const Notepad: React.FC<Props> = ({ id, initialText = '' }) => {
     };
   }, [value, setMdText]);
 
+  const saveTextHandler = () => {
+    if (value || value?.trim() === '') {
+      return;
+    }
+
+    (onSave as Function)(mdText);
+  };
+
   return (
-    <TextEditor
-      value={value}
-      onChange={setValue}
-      preview="edit"
-      height={372}
-      previewOptions={{
-        rehypePlugins: [[rehypeSanitize]],
-      }}
-    />
+    <div className="min-h-[28rem]">
+      <TextEditor
+        value={value}
+        onChange={setValue}
+        preview="edit"
+        height={372}
+        previewOptions={{
+          rehypePlugins: [[rehypeSanitize]],
+        }}
+      />
+      {typeof onSave === 'function' && (
+        <Button className="block mx-auto mt-3" onClick={saveTextHandler}>
+          Save
+        </Button>
+      )}
+    </div>
   );
 };
 
