698282677022a70709fd23104e0228c8da46c58b
Tarwat <t.uddin9121@gmail.com>
2023-06-06 20:00:48 +0100

    feat: added hydration for journals

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,27 @@
+import { dehydrate, Hydrate } from '@tanstack/react-query';
+
+import getQueryClient from '@/app/getQueryClient';
 import CreateJournal from '@/components/dashboard/CreateJournal';
 import Journals from '@/components/dashboard/Journals';
+import { getJournals } from '@/services';
 
-const Dashboard = () => (
-  <main className="min-h-screen p-12">
-    <div className="flex items-center justify-between max-w-3xl mx-auto">
-      <h1>Dashboard</h1>
-      <CreateJournal />
-    </div>
-    <Journals />
-  </main>
-);
+const Dashboard = async () => {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(['user-journals'], getJournals);
+  // @ts-expect-error Server Component
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <Hydrate state={dehydratedState}>
+      <main className="min-h-screen p-12">
+        <div className="flex items-center justify-between max-w-3xl mx-auto">
+          <h1>Dashboard</h1>
+          <CreateJournal />
+        </div>
+        <Journals />
+      </main>
+    </Hydrate>
+  );
+};
 
 export default Dashboard;
