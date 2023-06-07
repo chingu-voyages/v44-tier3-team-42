1b896a3c91963176f1b47cb199f9e3accbb63780
Remi-dee <oluwaremidaniel36@gmail.com>
2023-06-07 01:15:53 -0700

    file changes reverted

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
